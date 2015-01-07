58d13c229e658afb0b656a41614a608db8fc81d4
lexie1011 <blch931011@163.com>
2015-01-07 20:18:58 +0800

    细节
    修改了一些细节

--- js.js
+++ js.js
@@ -1,3 +1,4 @@
+
 function byId(id)
     {
         return document.getElementById(id);
@@ -5,8 +6,9 @@ function byId(id)
 var cont = byId('contbox').getElementsByTagName('div');
 var nav = byId('btphoto').getElementsByTagName('li');
 var arr = byId('arrow');
+var l=nav.length;
 var getCurIndex = function(){
-    for(var i = 0; i < nav.length; i++){
+    for(var i = 0; i < l; i++){
         if(nav[i].className.indexOf('selected')!=-1)
             return i;
     }
@@ -16,19 +18,19 @@ var move = function(flag){
     index = getCurIndex();
     if(flag){
         index++;
-        if(index >= cont.length)
+        if(index >= l)
             index = 0;
     }
     else{
         index--;
         if(index < 0)
-        index = cont.length-1;
+            index = l - 1;
     }
     display(index);
 }
 
 var display = function(index){
-    for (var i = 0; i < cont.length; i++) {
+    for (var i = 0; i < l; i++) {
         cont[i].className = '';
         nav[i].className = '';
     }
@@ -36,20 +38,26 @@ var display = function(index){
     nav[index].className = 'selected';
 }
 var Timehand;
-var autoTabStart = function(){
-    Timehand = setInterval(function(){move(true);},2000);
+var autoStart = function(){
+    Timehand = setInterval(function(){
+        move(true);
+    },2000);
 }
-var autoTapStop = function(){
+var autoStop = function(){
     clearInterval(Timehand);
 }
 var showOrHidden = function(flag) {
-    flag ? arr.style.display = 'block': arr.style.display = 'none';             
+    if(flag)
+        arr.style.display = 'block';
+
+    else 
+        arr.style.display = 'none';             
 }
 var showAndStop = function(){
     showOrHidden(true);
-    autoTapStop();
+    autoStop();
 }
 var hiddenAndStart = function(){
     showOrHidden(false);
-    autoTabStart();
+    autoStart();
 }
